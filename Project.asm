997b33026e77c9988919dafaf0963d4ade64755b
hamedkhaledi <46817485+hamedkhaledi@users.noreply.github.com>
2021-03-21 06:34:38 +0330

    add func for moving down

--- Project.asm
+++ Project.asm
@@ -21,9 +21,26 @@ title (exe) Graphics System Calls
      
      cell_x dw 5
      cell_y dw 4
-     cell_color db 1
+     cell_color db 1    
      
+     cell_x_index dw 0
+     cell_y_index dw 0
+     cell_x_pos dw 5
+     cell_y_pos dw 4
+     
+     is_down_empty db 0
+     cell_x_down dw 0
+     cell_y_down dw 0
+     
+     cell_x_color_get dw 0 ; for get_color_of_cell (get color of specific cell)
+     cell_y_color_get dw 0 
+     cell_color_get db 0   
+     
+     check_cell_x dw 0
+     check_cell_y dw 0
+     check_is_empty dw 0
      seconds db 0  ;?¦¦ VARIABLE IN DATA SEGMENT.
+     
 .code 
 
 
@@ -32,23 +49,27 @@ main proc far
     mov ds, ax   
     call clear_screen    
     call set_graphic_mode 
-    call color_cell
     call create_background
     mov bx, 0    
     
-for_temp:
-    mov cell_color,0
-    push bx  
-    call delay
-    call color_cell   
-    pop bx 
-    push bx
-    mov cell_x , bx 
-    mov cell_color,1 
-    call color_cell   
-    pop bx    
-    inc bx  
-    cmp bx,11
+    mov cell_x, 0
+    mov cell_y, 4
+    mov cell_color, 1  
+    call color_cell  
+    
+    mov cell_x, 5
+    mov cell_y, 4
+    mov cell_color, 3  
+    call color_cell
+
+    mov cell_y_down, 4 
+
+for_temp:  
+    call delay 
+    call move_down_cell   
+    inc bx   
+    mov cell_x_down, bx
+    cmp bx,20
     jnz for_temp
 
     mov ax, 4c00h ; exit to operating system.
@@ -143,11 +164,12 @@ loop_horizontal1:
     ret   
 endp draw_horizontal_line   
 
-;;;;;;;;;;;;;;;;;
+;;;;;;change color of specific cell;;;;;;;;;;;
 color_cell proc   
     push ax 
     push bx
-    push cx
+    push cx         
+    
     mov ax, cell_x
     mov cx, 16
     mul cx      
@@ -199,50 +221,152 @@ loop2:
     
     ret
     
-endp draw_solid_rectangle 
-
-;;;;;;;;;;;;;;;;;
-shift_rectangle proc
-    mov ah, 0ch 
-    mov al, 0
-    mov dx, start_row_rec
-    mov cx, start_col_rec 
-loop3:   
-    int 10h
-    inc dx
-    cmp dx, finish_row_rec
-    jnz loop3
-              
-              
-    mov al, 1
-    mov dx, start_row_rec
-    mov cx, finish_col_rec 
-loop4:
-    int 10h    
-    inc dx
-    cmp dx, finish_row_rec
-    jnz loop4         
-    
-    inc start_col_rec
-    inc finish_col_rec    
-    
-    ret                        
-endp shift_rectangle                                
-
+endp draw_solid_rectangle                                
 ;;;;;;;;;;;;;;;;; 
-
-
-delay proc  
+delay proc   
+  push ax
+  push bx
+  push cx
+  push dx 
+  
 delaying:   
-;GET SYSTEM TIME.
   mov  ah, 2ch
-  int  21h      ;?¦¦ RETURN SECONDS IN DH.
-;CHECK IF ONE SECOND HAS PASSED. 
-  cmp  dh, seconds  ;?¦¦ IF SECONDS ARE THE SAME...
-  je   delaying     ;    ...WE ARE STILL IN THE SAME SECONDS.
-  mov  seconds, dh  ;?¦¦ SECONDS CHANGED. PRESERVE NEW SECONDS.
+  int  21h      
+  cmp  dh, seconds  
+  je   delaying     
+  mov  seconds, dh
+    
+  pop dx
+  pop cx
+  pop bx
+  pop ax
   ret
 delay endp 
+;;;;;;;find center position of a cell;;;;;;;;;; 
+find_cell_center proc
+    push ax 
+    push bx
+    push cx
+    mov ax, cell_x_index
+    mov cx, 16
+    mul cx      
+    mov bx, start_row
+    add ax, bx  
+    add ax, 8
+    mov cell_x_pos, ax
+    
+    mov ax, cell_y_index 
+    mov cx, 16
+    mul cx
+    mov bx, start_col
+    add ax, bx
+    add ax, 8
+    mov cell_y_pos, ax 
+ 
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp find_cell_center 
+
 ;;;;;;;;;;;;;;;;;
+move_down_cell proc ;todo
+    push ax 
+    push bx
+    push cx
+    push dx
+    
+    
+    mov ax, cell_x_down  
+    mov check_cell_x, ax
+    inc check_cell_x  
+    
+    mov ax, cell_y_down  
+    mov check_cell_y, ax
+    
+    call check_cell_empty
+    cmp  check_is_empty, 0
+    je   done       
+    
+    mov ax, cell_x_down
+    mov cell_x, ax  
+    mov cell_x_color_get, ax 
+    mov ax, cell_y_down
+    mov cell_y, ax
+    mov cell_y_color_get, ax 
+    
+    call get_color_of_cell
+    mov al, cell_color_get   
+    push ax
+    mov cell_color,0
+    call color_cell   
+    inc cell_x
+    pop ax  
+    mov cell_color,al 
+    call color_cell
+done:       
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp move_down_cell    
+;;;;;;get color of specific cell;;;;;;;;;;;
+get_color_of_cell proc
+    push ax 
+    push bx
+    push cx
+    push dx
+    
+
+    mov ax, cell_x_color_get
+    mov cell_x_index,ax 
+    mov ax, cell_y_color_get
+    mov cell_y_index,ax
+    call find_cell_center 
+    
+    mov ah, 0dh 
+    mov dx, cell_x_pos 
+    mov cx, cell_y_pos
+    int 10h
+    mov cell_color_get, al
+     
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp get_color_of_cell    
+;;;;;;check a cell;;;;;;;;;;;
+check_cell_empty proc
+    push ax 
+    push bx
+    push cx
+    push dx
+    
+    mov check_is_empty, 0
+    mov ax, check_cell_x 
+    mov bx, check_cell_y 
+      
+    cmp ax, 11
+    jge else_check_cell
+    cmp bx, 8
+    jge else_check_cell 
+    
+    mov cell_x_color_get, ax
+    mov cell_y_color_get, bx
+    call get_color_of_cell 
+    mov al, cell_color_get
+    cmp al, 0
+    jne else_check_cell 
    
+    mov check_is_empty, 1 
+else_check_cell:
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp check_cell_empty 
+;;;;;;;;;;;;;;;;;
 end main
