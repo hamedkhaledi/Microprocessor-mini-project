771f388b3866aff4b6ae2dbb3651015c23243b1f
hamedkhaledi <46817485+hamedkhaledi@users.noreply.github.com>
2021-03-26 10:43:07 +0430

    add predicted block ...

--- Project.asm
+++ Project.asm
@@ -1,7 +1,9 @@
 title (exe) Graphics System Calls 
 
 .model small 
-.stack 64   
+.stack 64  
+ 
+  
 
 .data 
      start_col dw   100 
@@ -61,48 +63,82 @@ title (exe) Graphics System Calls
      check_block_rotate dw 0 
      
      rand_num_in dw 0
-     rand_num_out dw 0  
+     rand_num_out dw 0   
+     
+     score dw 0   
+     removed_lines dw 0
+     msg1 db "0"
+     number_printed db 0
+     
+     moving_block_hint_x dw 0, 0, 0, 0
+     moving_block_hint_y dw 0, 0, 0, 0
+     
+     rand1 dw 6
+     rand2 dw 6
+     line_color db 8
+     
+     predict_cell_x dw 0
+     rand_predict dw 0
+     
+#start=led_display.exe#
      
 .code 
-
 
 main proc far
     mov ax, @data
     mov ds, ax   
+    call start_game
+
+main endp 
+;;;;;;;;;;;;;;;;;
+proc start_game 
+    
+    mov rand_num_in, 5
+    call rand_gen
+    mov ax, rand_num_out 
+    mov rand1, ax
+         
     call clear_screen    
-    call set_graphic_mode 
+    call set_graphic_mode
+    
+    mov ax,110 ;test
+    out 199, ax
+    
+    mov line_color, 8 
     call create_background
-   
-      
+    call delay
+    mov rand_num_in, 5
+    call rand_gen
+    mov ax, rand_num_out 
+    mov rand2, ax 
+
+    
     call gen_new_block
-    
-    
+    mov score, 0   
+    call delay
     mov bx, 0 
-for_temp:
+for_temp:     
     call check_key_pressed
     call check_key_pressed
     call check_key_pressed
     call check_key_pressed 
-    mov ah,0c ; clear keyboard buffer
+    mov ah,0ch ; clear keyboard buffer
     mov al,0
-    int 21h
+    int 21h               
+    call show_hint
     call delay
     call mov_down_block
     cmp check_block_down, 1
     je not_gen_new_block_for 
-    call clear_lines
-    call gen_new_block   
+    call clear_lines  
+    call print_score
+    call gen_new_block 
+    mov ah,0ch ; clear keyboard buffer
+    mov al,0
 not_gen_new_block_for:
-    inc bx   
-    cmp bx,45
-    jnz for_temp
+    jmp for_temp
 
-    
-    mov ax, 4c00h ; exit to operating system.
-    int 21h    
-
-main endp
-
+start_game endp 
 ;;;;;;;;;;;;;;;;;
 clear_screen proc 
     push ax 
@@ -188,7 +224,7 @@ draw_vertical_line proc
     push dx
     
     mov ah, 0ch 
-    mov al, 8
+    mov al, line_color
      
     mov dx, start_row_line  
     mov cx, start_col_line
@@ -196,7 +232,7 @@ loop_vertical1:
     int 10h
     inc dx
     cmp dx, finish_row_line
-    jnz loop_vertical1 
+    jl loop_vertical1 
     
     pop dx
     pop cx
@@ -214,7 +250,7 @@ draw_horizontal_line proc
     push dx
     
     mov ah, 0ch 
-    mov al, 8
+    mov al, line_color
      
     mov dx, start_row_line  
     mov cx, start_col_line
@@ -222,7 +258,7 @@ loop_horizontal1:
     int 10h
     inc cx
     cmp cx, finish_col_line
-    jnz loop_horizontal1 
+    jl loop_horizontal1 
     
     pop dx
     pop cx
@@ -243,10 +279,9 @@ color_cell proc
     mul cx      
     mov bx, start_row
     add ax, bx  
-    add ax, 1
     mov start_row_rec, ax
     
-    add ax, 15
+    add ax, 16
     mov finish_row_rec, ax
      
     mov ax, cell_y 
@@ -254,13 +289,14 @@ color_cell proc
     mul cx
     mov bx, start_col
     add ax, bx
-    add ax, 1
     mov start_col_rec, ax 
     
-    add ax, 15
+    add ax, 16
     mov finish_col_rec, ax   
 
-    call draw_solid_rectangle
+    call draw_solid_rectangle 
+    mov line_color, 8
+    call color_border_cell 
     pop dx   
     pop cx
     pop bx
@@ -268,7 +304,52 @@ color_cell proc
     
     ret
 endp color_cell 
+;;;;;;change border color of specific cell;;;;;;;;;;;
+color_border_cell proc   
+    push ax 
+    push bx
+    push cx  
+    push dx        
+    
+    mov ax, cell_x
+    mov cx, 16
+    mul cx      
+    mov bx, start_row
+    add ax, bx  
+    mov start_row_line, ax
+    
+    add ax, 16
+    mov finish_row_line, ax
+     
+    mov ax, cell_y 
+    mov cx, 16
+    mul cx
+    mov bx, start_col
+    add ax, bx
+    mov start_col_line, ax 
+    
+    add ax, 16
+    mov finish_col_line, ax   
 
+    call draw_vertical_line
+    call draw_horizontal_line 
+    
+    mov ax, finish_row_line 
+    mov bx, start_row_line 
+    mov start_row_line, ax
+    call draw_horizontal_line
+    mov start_row_line, bx 
+    
+    mov ax, finish_col_line  
+    mov start_col_line, ax
+    call draw_vertical_line 
+    pop dx   
+    pop cx
+    pop bx
+    pop ax
+    
+    ret
+endp color_border_cell 
 ;;;;;;;;;;;;;;;;;
 draw_solid_rectangle proc
 
@@ -509,8 +590,16 @@ next4_key_pressed:
     jne next5_key_pressed
     call mov_to_end
 
-next5_key_pressed:
-
+next5_key_pressed:   
+    cmp al, 'q'
+    jne next6_key_pressed
+    mov ax, 4c00h ; exit to operating system.
+    int 21h 
+     
+next6_key_pressed:
+    cmp al, 'r'
+    jne done_key_pressed
+    call game_done    
 done_key_pressed:
  
     pop dx
@@ -519,7 +608,7 @@ done_key_pressed:
     pop ax
     ret
 endp check_key_pressed    
-;;;;;;;mov down current block;;;;;;;;;;
+;;;;;;;mov down until end current block;;;;;;;;;;
 mov_down_block proc
     push ax 
     push bx
@@ -590,6 +679,7 @@ mov_down_block_common proc
     push bx
     push cx
     push dx  
+    push si
     
     mov cx, 0
 for1_mov_down_block:     
@@ -629,6 +719,7 @@ done_for2_mov_down_block:
     
     add moving_block_center_x, 16 
     
+    pop si
     pop dx
     pop cx
     pop bx
@@ -1054,8 +1145,8 @@ rand_gen proc
     push cx
     push dx 
     
-    MOV AH, 00h  ; interrupts to get system time        
-    INT 1AH      ; CX:DX now hold number of clock ticks since midnight      
+    MOV AH, 2ch  ; interrupts to get system time        
+    INT 21H      ; CX:DX now hold number of clock ticks since midnight      
     
     mov  ax, dx
     xor  dx, dx
@@ -1076,12 +1167,16 @@ gen_new_block proc
     push bx
     push cx
     push dx 
+
     
+    mov ax, rand1
+    mov bx, rand2
+    mov rand1,bx    
     mov rand_num_in, 5
-    call rand_gen
-    mov ax, rand_num_out
+    mov bx, rand_num_out 
+    mov rand2, bx
     
-    
+
     cmp ax,0
     jnz next1_new_block 
     call new_block1_conf
@@ -1108,13 +1203,169 @@ next4_new_block:
  
     call new_block5_conf
     
-done_gen_new_block:       
+done_gen_new_block:
+    call clear_predict_area 
+    mov ax, rand1
+    mov rand_predict, ax
+    mov predict_cell_x,2  
+    call predict_block 
+    mov ax, rand2
+    mov rand_predict, ax
+    mov predict_cell_x,6  
+    call predict_block      
     pop dx
     pop cx
     pop bx
     pop ax
     RET
-endp gen_new_block   
+endp gen_new_block
+;;;;;;;;;;;;clear predict area;;;;;;;;;;;;;;;;; 
+clear_predict_area proc         
+    push ax 
+    push bx
+    push cx
+    push dx   
+         
+    mov ax,9
+    mov bx,2
+    mov cell_color, 15
+clear_predict_for1: 
+    cmp ax, 13
+    jge done_clear_predict_for1
+    mov bx, 2   
+clear_predict_for2:
+    cmp bx, 9
+    jge done_clear_predict_for2
+    mov cell_x, bx
+    mov cell_y, ax     
+    call color_cell
+    inc bx
+    jmp clear_predict_for2    
+done_clear_predict_for2:  
+    inc ax
+    jmp clear_predict_for1
+done_clear_predict_for1:        
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    RET
+endp clear_predict_area
+;;;;;;;;;;;;generate a new block;;;;;;;;;;;;;;;;; 
+predict_block proc         
+    push ax 
+    push bx
+    push cx
+    push dx
+    mov ax, 9
+    mov bx, predict_cell_x 
+    
+    cmp rand_predict, 0   
+    jnz next1_predict_block
+    mov cell_color, 11  
+    mov cell_x, bx
+    mov cell_y, ax    
+    call color_cell 
+    inc ax    
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc ax  
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc ax             
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell
+    jmp done_predict_block
+next1_predict_block:      
+    cmp rand_predict, 1  
+    jnz next2_predict_block
+    mov cell_color, 14  
+    mov cell_x, bx
+    mov cell_y, ax    
+    call color_cell 
+    inc ax    
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc bx  
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    dec ax             
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell
+    jmp done_predict_block
+   
+next2_predict_block: 
+    cmp rand_predict, 2   
+    jnz next3_predict_block
+    mov cell_color, 12  
+    mov cell_x, bx
+    mov cell_y, ax    
+    call color_cell 
+    inc bx    
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc bx  
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc ax             
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell
+    jmp done_predict_block
+   
+next3_predict_block:
+    cmp rand_predict, 3   
+    jnz next4_predict_block
+    mov cell_color, 10  
+    mov cell_x, bx
+    mov cell_y, ax    
+    call color_cell 
+    inc bx    
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc ax  
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc bx             
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell
+    jmp done_predict_block   
+next4_predict_block:
+    mov cell_color, 13  
+    mov cell_x, bx
+    mov cell_y, ax    
+    call color_cell 
+    inc ax    
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc bx  
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell 
+    inc ax
+    dec bx             
+    mov cell_x, bx
+    mov cell_y, ax
+    call color_cell
+done_predict_block:     
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    RET
+endp predict_block1
 ;;;;;;;;;;;;generate a new block;;;;;;;;;;;;;;;;; 
 new_block1_conf proc         
     push ax 
@@ -1129,10 +1380,13 @@ new_block1_conf proc
     call rand_gen
     mov ax, rand_num_out 
     
+    
+    
     mov [moving_block_x], 0  
     mov [moving_block_y], ax  
     mov cell_x, 0
-    mov cell_y, ax
+    mov cell_y, ax  
+    call check_game_done    
     call color_cell 
     inc ax    
     
@@ -1151,6 +1405,7 @@ new_block1_conf proc
     mov [moving_block_y + 2], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell 
     inc ax 
              
@@ -1159,12 +1414,15 @@ new_block1_conf proc
     mov [moving_block_y + 4], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done 
     call color_cell 
-    inc ax 
+    inc ax            
+    
     mov [moving_block_x + 6], 0  
     mov [moving_block_y + 6], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell     
     
     pop dx
@@ -1192,6 +1450,7 @@ new_block2_conf proc
     mov [moving_block_y], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell   
     
     mov cell_x_index, 0
@@ -1209,17 +1468,22 @@ new_block2_conf proc
     mov [moving_block_y + 2], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell 
-    inc ax  
+    inc ax              
+    
     mov [moving_block_x + 4], 0  
     mov [moving_block_y + 4], ax  
     mov cell_x, 0
     mov cell_y, ax
-    call color_cell 
+    call check_game_done
+    call color_cell     
+    
     mov [moving_block_x + 6], 1  
     mov [moving_block_y + 6], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell     
     
     pop dx
@@ -1236,8 +1500,8 @@ new_block3_conf proc
     push dx                  
     
     mov moving_block_mode, 2
-    mov moving_block_color, 4
-    mov cell_color, 4
+    mov moving_block_color, 12
+    mov cell_color, 12
      
     mov rand_num_in, 7
     call rand_gen
@@ -1246,12 +1510,15 @@ new_block3_conf proc
     mov [moving_block_x], 0  
     mov [moving_block_y], ax  
     mov cell_x, 0
-    mov cell_y, ax
-    call color_cell 
+    mov cell_y, ax 
+    call check_game_done
+    call color_cell     
+    
     mov [moving_block_x + 2], 1  
     mov [moving_block_y + 2], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell  
     
     mov cell_x_index, 1
@@ -1266,12 +1533,15 @@ new_block3_conf proc
     mov [moving_block_y + 4], ax  
     mov cell_x, 2
     mov cell_y, ax
+    call check_game_done
     call color_cell 
-    inc ax
+    inc ax              
+    
     mov [moving_block_x + 6], 2  
     mov [moving_block_y + 6], ax  
     mov cell_x, 2
     mov cell_y, ax
+    call check_game_done
     call color_cell     
     
     pop dx
@@ -1299,17 +1569,22 @@ new_block4_conf proc
     mov [moving_block_y], ax  
     mov cell_x, 0
     mov cell_y, ax
-    call color_cell 
+    call check_game_done
+    call color_cell     
+    
     mov [moving_block_x + 2], 1  
     mov [moving_block_y + 2], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell   
-    inc ax
+    inc ax              
+    
     mov [moving_block_x + 4], 1  
     mov [moving_block_y + 4], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell 
     
     
@@ -1325,6 +1600,7 @@ new_block4_conf proc
     mov [moving_block_y + 6], ax  
     mov cell_x, 2
     mov cell_y, ax
+    call check_game_done
     call color_cell     
     
     pop dx
@@ -1352,12 +1628,15 @@ new_block5_conf proc
     mov [moving_block_y], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell     
-    inc ax
+    inc ax            
+    
     mov [moving_block_x + 2], 0  
     mov [moving_block_y + 2], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell
     
     mov cell_x_index, 0
@@ -1373,12 +1652,15 @@ new_block5_conf proc
     mov [moving_block_y + 4], ax  
     mov cell_x, 1
     mov cell_y, ax
+    call check_game_done
     call color_cell   
     inc ax
+    
     mov [moving_block_x + 6], 0  
     mov [moving_block_y + 6], ax  
     mov cell_x, 0
     mov cell_y, ax
+    call check_game_done
     call color_cell     
     
     pop dx
@@ -1394,11 +1676,11 @@ proc clear_lines
     push cx
     push dx
     
-    mov bx,10
-    
+    mov bx,0
+    mov removed_lines,0 
 clear_lines_for1:
-    cmp bx,0
-    jl done_clear_lines_for1
+    cmp bx,10
+    jg done_clear_lines_for1
     mov cx,0
 clear_lines_for2:        
     mov check_cell_x, bx
@@ -1409,9 +1691,10 @@ clear_lines_for2:
     
     inc cx
     cmp cx, 8
-    jl clear_lines_for2
+    jl clear_lines_for2 
+    inc removed_lines
     mov cx, 0
-clear_lines_for3:  
+clear_lines_for3:     
     mov cell_x, bx
     mov cell_y, cx
     mov cell_color, 0
@@ -1430,15 +1713,230 @@ clear_lines_for4:
     cmp cx, 8
     jl clear_lines_for3  
 done_clear_lines_for2:          
-    dec bx
+    inc bx
     jmp clear_lines_for1
 done_clear_lines_for1:
+    cmp removed_lines, 1
+    jg next_removed_lines 
+    cmp removed_lines, 0
+    je done_clear_lines  
+    add score, 10
+    jmp done_clear_lines
+next_removed_lines:
+    mov ax, removed_lines
+    mov cx, 20
+    mul cx
+    add score, ax
+done_clear_lines:
     pop dx
     pop cx
     pop bx
     pop ax
     RET    
-endp clear_lines  
+endp clear_lines        
+;;;;;;;;;;;;;;;;;;;;; 
+check_game_done proc 
+    push ax 
+    push bx
+    push cx
+    push dx
+    
+    mov ax, cell_x
+    mov bx, cell_y 
+    mov check_cell_x, ax
+    mov check_cell_y, bx
+    call check_cell_empty
+    cmp check_is_empty, 1
+    je not_done_game
+    call game_done 
+not_done_game:    
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    RET    
+     
+endp not_done_game_1: 
+;;;;;;; print score;;;;;;;;;;;;;;
+print_score PROC  
+    push ax 
+    push bx
+    push cx
+    push dx
+    push si
+    push es      
+    
+    push ds
+    pop es   
+    
+    mov ax,score
+    out 199, ax      
+    
+    mov cx,0 
+    mov dx,0   
+    mov number_printed, 0
+    label1: 
+        cmp ax,0 
+        je print1       
+        mov bx,10         
+        div bx                    
+        push dx                
+        inc cx                 
+        xor dx,dx 
+        jmp label1 
+    print1:  
+        cmp cx,0 
+        je exit
+        pop dx 
+        add dx,48   
+        mov [msg1], dl   
+        
+        inc number_printed
+          
+ 
+        
+        push ax 
+        push bx
+        push cx
+        push dx   
+        ; print message using bios int 10h/13h function
+        mov al, 0
+        mov bh, 0
+        mov bl, 0011_1111b
+        mov cx, 1
+        mov dl, number_printed
+        mov dh, 12  
+        
+        mov bp, offset msg1
+        mov ah, 13h
+        int 10h  
+
+        pop dx
+        pop cx
+        pop bx
+        pop ax  
+        ;decrease the count 
+        dec cx 
+        jmp print1 
+exit: 
+    
+    pop es
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    RET    
+     
+print_score ENDP    
+;;;;;;;;;;;;;;;;;;;;;
+game_done proc   
+    call delay
+    call delay 
+    call delay  
+    call start_game
+endp game_done  
+;;;;;;;show hint;;;;;;;;;;
+show_hint proc
+    push ax 
+    push bx
+    push cx
+    push dx 
+    push si 
+    
+    mov cx,0
+    mov line_color, 8 
+for0_show_hint:     
+    cmp cx, 4
+    jge done_for0_show_hint
+    mov si, cx
+    add si, cx
+    inc cx 
+    mov ax, [moving_block_hint_x + si]
+    mov bx, [moving_block_hint_y + si]
+    mov cell_x, ax
+    mov cell_y, bx 
+    call color_border_cell  
+    jmp for0_show_hint
+done_for0_show_hint:
+         
+    mov cx,0 
+for1_show_hint:     
+    cmp cx, 4
+    jge done_for1_show_hint
+    mov si, cx
+    add si, cx
+    inc cx 
+    mov ax, [moving_block_x + si]
+    mov bx, [moving_block_y + si]
+    mov [moving_block_hint_x + si], ax
+    mov [moving_block_hint_y + si], bx  
+    jmp for1_show_hint   
+done_for1_show_hint:
+    
+    call is_down_block_empty 
+    cmp check_block_down, 0
+    je show_hint_done 
+    
+show_hint_while: 
+    mov cx, 0
+for2_show_hint:     
+    cmp cx, 4
+    jge done_for2_show_hint
+    mov si, cx
+    add si, cx
+    inc cx 
+    inc [moving_block_x + si]  
+    jmp for2_show_hint  
+done_for2_show_hint:  
+    call is_down_block_empty
+    cmp check_block_down, 1
+    je show_hint_while  
+  
+show_hint_done:   
+
+mov cx,0  
+
+for3_show_hint:     
+    cmp cx, 4
+    jge done_for3_show_hint
+    mov si, cx
+    add si, cx
+    inc cx 
+    mov ax, [moving_block_x + si]
+    mov bx, [moving_block_y + si]
+    mov cell_x, ax
+    mov cell_y, bx
+    mov dl,moving_block_color
+    mov line_color, dl 
+    call color_border_cell  
+    
+    mov ax, [moving_block_hint_x + si]
+    mov bx, [moving_block_hint_y + si]
+    mov cell_x, ax
+    mov cell_y, bx 
+    mov line_color, 8
+    call color_border_cell
+    
+    mov ax, [moving_block_hint_x + si]
+    mov bx, [moving_block_x + si] 
+    mov [moving_block_x + si], ax
+    mov [moving_block_hint_x + si], bx 
+    mov ax, [moving_block_hint_y + si]
+    mov bx, [moving_block_y + si] 
+    mov [moving_block_y + si], ax
+    mov [moving_block_hint_y + si], bx 
+    
+    jmp for3_show_hint     
+done_for3_show_hint: 
+    
+    pop si                   
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+endp show_hint
 ;;;;;;;;;;;;;;;;;;;;;
 end main                            
 
